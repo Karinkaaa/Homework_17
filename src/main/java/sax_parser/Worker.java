aef1f012b03f6fc4a2c5c0489f5ed839719fd665
karin <karinka.kostraba@gmail.com>
2019-05-25 23:21:12 +0300

    conversion XML-file to List

---
+++ src/main/java/sax_parser/Worker.java
@@ -0,0 +1,57 @@
+package sax_parser;
+
+public class Worker {
+
+    private String login;
+    private String position;
+    private String company;
+    private int sinceYear;
+
+    public Worker(String login, String position, String company, int sinceYear) {
+        this.login = login;
+        this.position = position;
+        this.company = company;
+        this.sinceYear = sinceYear;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getSinceYear() {
+        return sinceYear;
+    }
+
+    public void setSinceYear(int sinceYear) {
+        this.sinceYear = sinceYear;
+    }
+
+    @Override
+    public String toString() {
+
+       StringBuffer sb = new StringBuffer("WORKER:\n");
+       sb.append("Login: ").append(this.login).append("\nPosition: ").append(this.position);
+       sb.append("\nCompany: ").append(this.company).append("\nSince year: ").append(this.sinceYear);
+       return sb.toString();
+    }
+}
